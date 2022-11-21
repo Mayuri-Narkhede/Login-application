b20237eebabacc218a2aa4ef5a63929ea9cda539
Mayuri-Narkhede <107295739+Mayuri-Narkhede@users.noreply.github.com>
2022-11-21 19:03:54 +0530

    added page to upload file

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/file" element={<File />} />
       </Routes>
     </BrowserRouter>
   )
